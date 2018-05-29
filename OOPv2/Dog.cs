e76e7d8a7ea7ff255382987821e98a74de78723a
unknown <t.axford2@yahoo.com>
2018-05-29 13:17:23 -0400

    Added Dog class with fields, constructors

---
+++ OOPv2/Dog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPv2
+{
+    class Dog
+    {
+        private int hairLength;
+        private int height;
+        private double runningSpeed;
+        private double weight;
+        private bool Run = true;
+        private bool Bark = true;
+        private bool Potty = true;
+        private bool Cuddle = true;
+
+        //fields
+        public int HairLength
+        {
+            get { return this.hairLength; }
+            set { this.hairLength = value; }
+        }
+
+        public int Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public double RunningSpeed
+        {
+            get { return this.runningSpeed; }
+            set { this.runningSpeed = value; }
+        }
+
+        public double Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        //Cunstructor
+        public Dog()
+        {
+            //default constructor
+        }
+
+        public Dog(int hairLength, int height, double runningSpeed, double weight)
+        {
+            this.hairLength = hairLength;
+            this.height = height;
+            this.runningSpeed = runningSpeed;
+            this.weight = weight;
+        }
+
+    }
+}
--- OOPv2/Program.cs
+++ OOPv2/Program.cs
@@ -4,7 +4,7 @@ namespace OOPv2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             
         }
