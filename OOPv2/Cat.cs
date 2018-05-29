ceb9232740c8ebdb68eeb2e0af3a003c1b955fa1
unknown <t.axford2@yahoo.com>
2018-05-29 11:58:23 -0400

    Added variables, fields, and constructors to Cat Class

---
+++ OOPv2/Cat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPv2
+{
+    class Cat
+    {
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //field
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public Cat()
+        {
+            //default constructor
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+    }
+}
